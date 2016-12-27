29767e94a46a9e09100d619769e70815d38d3fb2
Kevin Paulisse <kpaulisse@github.com>
2016-12-27 15:06:48 -0500

    Use shared examples for these tests

--- spec/octocatalog-diff/tests/catalog-diff/cli/options/environment_spec.rb
+++ spec/octocatalog-diff/tests/catalog-diff/cli/options/environment_spec.rb
@@ -4,28 +4,6 @@ require_relative '../options_helper'
 
 describe OctocatalogDiff::CatalogDiff::Cli::Options do
   describe '#opt_environment' do
-    it 'should handle --environment' do
-      result = run_optparse(['--environment', 'fizzbuzz'])
-      expect(result[:from_environment]).to eq('fizzbuzz')
-      expect(result[:to_environment]).to eq('fizzbuzz')
-    end
-
-    it 'should handle --to-environment' do
-      result = run_optparse(['--to-environment', 'fizzbuzz'])
-      expect(result[:from_environment]).to be_nil
-      expect(result[:to_environment]).to eq('fizzbuzz')
-    end
-
-    it 'should handle --from-environment' do
-      result = run_optparse(['--from-environment', 'fizzbuzz'])
-      expect(result[:from_environment]).to eq('fizzbuzz')
-      expect(result[:to_environment]).to be_nil
-    end
-
-    it 'should handle --from-environment + --to-environment' do
-      result = run_optparse(['--from-environment', 'fizzbuzz', '--to-environment', 'production'])
-      expect(result[:from_environment]).to eq('fizzbuzz')
-      expect(result[:to_environment]).to eq('production')
-    end
+    include_examples 'global string option', 'environment', :environment
   end
 end
