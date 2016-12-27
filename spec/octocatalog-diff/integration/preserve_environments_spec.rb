cc75054e2cc34a0f648c7bbd35794c177cb1bd7c
Kevin Paulisse <kpaulisse@github.com>
2016-12-27 13:10:23 -0500

    Add test case for --environments and --create-symlinks unset

--- spec/octocatalog-diff/integration/preserve_environments_spec.rb
+++ spec/octocatalog-diff/integration/preserve_environments_spec.rb
@@ -31,6 +31,32 @@ describe 'preserve environments integration' do
   end
 
   context 'with --preserve-environments set' do
+    context 'and --environment, --create-symlinks unset' do
+      before(:all) do
+        @result = OctocatalogDiff::Integration.integration(
+          spec_fact_file: 'facts.yaml',
+          spec_repo: 'preserve-environments',
+          argv: [
+            '-n', 'rspec-node.github.net',
+            '--retry-failed-catalog', '0',
+            '--preserve-environments'
+          ]
+        )
+      end
+
+      it 'should exit with error status' do
+        expect(@result.exitcode).to eq(-1), OctocatalogDiff::Integration.format_exception(@result)
+      end
+
+      it 'should raise OctocatalogDiff::CatalogDiff::Cli::Catalogs::CatalogError' do
+        expect(@result.exception).to be_a_kind_of(OctocatalogDiff::CatalogDiff::Cli::Catalogs::CatalogError)
+      end
+
+      it 'should fail because ::bar could not be located' do
+        expect(@result.exception.message).to match(/Could not find class ::bar for rspec-node.github.net/)
+      end
+    end
+
     context 'and --environment set to non-existent value' do
       before(:all) do
         @result = OctocatalogDiff::Integration.integration(
