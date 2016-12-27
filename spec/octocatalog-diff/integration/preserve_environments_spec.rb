b75ef40407e7ba0a273a05d2b06105a5da50c684
Kevin Paulisse <kpaulisse@github.com>
2016-12-27 15:15:22 -0500

    Add more test conditions

--- spec/octocatalog-diff/integration/preserve_environments_spec.rb
+++ spec/octocatalog-diff/integration/preserve_environments_spec.rb
@@ -2,31 +2,83 @@
 
 require_relative 'integration_helper'
 
-OctocatalogDiff::Spec.require_path('/catalog')
+OctocatalogDiff::Spec.require_path('/catalog-diff/cli/catalogs')
 
 describe 'preserve environments integration' do
   context 'without --preserve-environments set' do
-    before(:all) do
-      @result = OctocatalogDiff::Integration.integration(
-        spec_fact_file: 'facts.yaml',
-        spec_repo: 'preserve-environments',
-        argv: [
-          '-n', 'rspec-node.github.net',
-          '--retry-failed-catalog', '0'
-        ]
-      )
+    context 'without --environment set' do
+      before(:all) do
+        @result = OctocatalogDiff::Integration.integration(
+          spec_fact_file: 'facts.yaml',
+          spec_repo: 'preserve-environments',
+          argv: [
+            '-n', 'rspec-node.github.net',
+            '--retry-failed-catalog', '0'
+          ]
+        )
+      end
+
+      it 'should exit with error status' do
+        expect(@result.exitcode).to eq(-1), OctocatalogDiff::Integration.format_exception(@result)
+      end
+
+      it 'should raise OctocatalogDiff::CatalogDiff::Cli::Catalogs::CatalogError' do
+        expect(@result.exception).to be_a_kind_of(OctocatalogDiff::CatalogDiff::Cli::Catalogs::CatalogError)
+      end
+
+      it 'should fail because ::bar could not be located' do
+        expect(@result.exception.message).to match(/Could not find class ::bar for rspec-node.github.net/)
+      end
     end
 
-    it 'should exit with error status' do
-      expect(@result.exitcode).to eq(-1), OctocatalogDiff::Integration.format_exception(@result)
+    context 'with --environment set' do
+      before(:all) do
+        @result = OctocatalogDiff::Integration.integration(
+          spec_fact_file: 'facts.yaml',
+          spec_repo_old: 'default',
+          argv: [
+            '-n', 'rspec-node.github.net',
+            '--environment', 'asdfgh',
+            '--to-catalog', OctocatalogDiff::Spec.fixture_path('catalogs/default-catalog-v4.json'),
+            '--hiera-config', 'environments/production/config/hiera.yaml',
+            '--hiera-path-strip', '/var/lib/puppet'
+          ]
+        )
+      end
+
+      it 'should exit without error' do
+        expect(@result.exitcode).to eq(0), OctocatalogDiff::Integration.format_exception(@result)
+        expect(@result.exception).to be_nil, OctocatalogDiff::Integration.format_exception(@result)
+      end
+
+      it 'should log warning about --environment being useless in this context' do
+        expect(@result.logs).to match(/WARN -- : --environment is ignored unless --preserve-environment is used/)
+      end
     end
 
-    it 'should raise OctocatalogDiff::CatalogDiff::Cli::Catalogs::CatalogError' do
-      expect(@result.exception).to be_a_kind_of(OctocatalogDiff::CatalogDiff::Cli::Catalogs::CatalogError)
-    end
+    context 'with --create-symlinks set' do
+      before(:all) do
+        @result = OctocatalogDiff::Integration.integration(
+          spec_fact_file: 'facts.yaml',
+          spec_repo_old: 'default',
+          argv: [
+            '-n', 'rspec-node.github.net',
+            '--create-symlinks', 'asdfgh,asldfk',
+            '--to-catalog', OctocatalogDiff::Spec.fixture_path('catalogs/default-catalog-v4.json'),
+            '--hiera-config', 'environments/production/config/hiera.yaml',
+            '--hiera-path-strip', '/var/lib/puppet'
+          ]
+        )
+      end
 
-    it 'should fail because ::bar could not be located' do
-      expect(@result.exception.message).to match(/Could not find class ::bar for rspec-node.github.net/)
+      it 'should exit without error' do
+        expect(@result.exitcode).to eq(0), OctocatalogDiff::Integration.format_exception(@result)
+        expect(@result.exception).to be_nil, OctocatalogDiff::Integration.format_exception(@result)
+      end
+
+      it 'should log warning about --create-symlinks being useless in this context' do
+        expect(@result.logs).to match(/WARN -- : --create-symlinks is ignored unless --preserve-environment is used/)
+      end
     end
   end
 
