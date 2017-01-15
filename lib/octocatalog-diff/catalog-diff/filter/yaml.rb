50dd5f9a1e7342b491faa84964ee4024dd2ca1e4
Kevin Paulisse <kpaulisse@github.com>
2017-01-15 10:31:11 -0600

    Convert YAML filter to use diff API

--- lib/octocatalog-diff/catalog-diff/filter/yaml.rb
+++ lib/octocatalog-diff/catalog-diff/filter/yaml.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../../api/v1/diff'
 require_relative '../filter'
 
 require 'yaml'
@@ -13,20 +14,24 @@ module OctocatalogDiff
         # Return true if the YAML objects are known to be equivalent. Return false if they
         # are not equivalent, or if equivalence cannot be determined.
         #
-        # @param diff [Array] Difference
+        # @param diff_in [Array] Difference
         # @param _options [Hash] Additional options (there are none for this filter)
         # @return [Boolean] true if this difference is a YAML file with identical objects, false otherwise
-        def filtered?(diff, _options = {})
+        def filtered?(diff_in, _options = {})
+          # Create API object for diff to simplify code
+          diff = OctocatalogDiff::API::V1::Diff.new(diff_in)
+
           # Skip additions or removals - focus only on changes
-          return false unless diff[0] == '~' || diff[0] == '!'
+          return false unless diff.change?
 
           # Make sure we are comparing file content for a file ending in .yaml or .yml extension
-          return false unless diff[1] =~ /^File\f([^\f]+)\.ya?ml\fparameters\fcontent$/
+          return false unless diff.type == 'File' && diff.structure == %w(parameters content)
+          return false unless diff.title =~ /\.ya?ml\z/
 
-          # Attempt to convert the old (diff[2]) and new (diff[3]) into YAML objects. Assuming
+          # Attempt to convert the old value and new value into YAML objects. Assuming
           # that doesn't error out, the return value is whether or not they're equal.
-          obj_old = ::YAML.load(diff[2])
-          obj_new = ::YAML.load(diff[3])
+          obj_old = ::YAML.load(diff.old_value)
+          obj_new = ::YAML.load(diff.new_value)
           obj_old == obj_new
         rescue # Rescue everything - if something failed, we aren't sure what's going on, so we'll return false.
           false
