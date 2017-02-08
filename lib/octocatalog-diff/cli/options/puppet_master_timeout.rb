f74d06dcd6a35f0fdb4be4df8ae7d9b1f4616bff
Kevin Paulisse <kpaulisse@github.com>
2017-02-07 19:04:11 -0600

    Add CLI option for Puppet Master timeout

---
+++ lib/octocatalog-diff/cli/options/puppet_master_timeout.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Specify a timeout for retrieving a catalog from a Puppet master / Puppet server.
+# This timeout is specified in seconds.
+# @param parser [OptionParser object] The OptionParser argument
+# @param options [Hash] Options hash being constructed; this is modified in this method.
+OctocatalogDiff::Cli::Options::Option.newoption(:puppet_master_timeout) do
+  has_weight 329
+
+  def parse(parser, options)
+    OctocatalogDiff::Cli::Options.option_globally_or_per_branch(
+      parser: parser,
+      options: options,
+      cli_name: 'puppet-master-timeout',
+      option_name: 'puppet_master_timeout',
+      desc: 'Puppet Master catalog retrieval timeout in seconds',
+      validator: ->(x) { x.to_i > 0 || raise(ArgumentError, 'Specify timeout as a integer greater than 0') },
+      translator: ->(x) { x.to_i }
+    )
+  end
+end
