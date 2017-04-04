a335dfb4433915255499661caa8df9380d9fc4e5
Kevin Paulisse <kpaulisse@github.com>
2017-04-04 12:03:18 -0500

    Add example git submodule script

---
+++ examples/script-overrides/git-extract-submodules/git-extract.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script is called from lib/octocatalog-diff/catalog-util/git.rb and is used to
+# archive and extract a certain branch of a git repository into a target directory.
+
+if [ -z "$OCD_GIT_EXTRACT_BRANCH" ]; then
+  echo "Error: Must declare OCD_GIT_EXTRACT_BRANCH"
+  exit 255
+fi
+
+if [ -z "$OCD_GIT_EXTRACT_TARGET" ]; then
+  echo "Error: Must declare OCD_GIT_EXTRACT_TARGET"
+  exit 255
+fi
+
+set -euf -o pipefail
+git clone --single-branch -b "$OCD_GIT_EXTRACT_BRANCH" . "$OCD_GIT_EXTRACT_TARGET"
+( cd "$OCD_GIT_EXTRACT_TARGET" && git submodule init && git submodule sync  && git submodule update )
