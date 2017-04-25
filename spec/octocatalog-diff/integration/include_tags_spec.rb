7c02f8a71f5c296dd2565da57be7d61fa555cdb8
Kevin Paulisse <kpaulisse@github.com>
2017-04-25 16:59:43 -0400

    Stub include-tags integration

---
+++ spec/octocatalog-diff/integration/include_tags_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'integration_helper'
+require 'json'
+
+describe 'include-tags integration' do
+  context 'with --include-tags specified' do
+  end
+
+  context 'with --no-include-tags specified' do
+  end
+
+  context 'with --include-tags not specified' do
+  end
+
+  context 'with --ignore-tags and --include-tags specified' do
+  end
+
+  context 'with --ignore-tags and --no-include-tags specified' do
+  end
+end
