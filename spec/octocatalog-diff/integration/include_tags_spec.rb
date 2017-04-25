37c64b5442d027a322b5809ff4aee8acbdf33b30
Kevin Paulisse <kpaulisse@github.com>
2017-04-25 17:17:04 -0400

    Add remaining integration test cases

--- spec/octocatalog-diff/integration/include_tags_spec.rb
+++ spec/octocatalog-diff/integration/include_tags_spec.rb
@@ -32,19 +32,116 @@ describe 'include-tags integration' do
       expect(diffs.first.change?).to eq(true)
       expect(diffs.first.structure).to eq(['tags'])
       expect(diffs.first.old_value).to eq(['tag-one', 'tag-two'])
-      expect(diffs.first.new_value).to eq(['tag-one-new', 'tag-too'])
+      expect(diffs.first.new_value).to eq(['ignore-tag__file', 'tag-one-new', 'tag-too'])
     end
   end
 
   context 'with --no-include-tags specified' do
+    let(:argv) do
+      default_argv.concat ['--no-include-tags']
+    end
+
+    let(:result) do
+      OctocatalogDiff::Integration.integration(
+        argv: argv
+      )
+    end
+
+    it 'should exit indicating success with no differences' do
+      expect(result.exitcode).to eq(0)
+    end
+
+    it 'should contain representative tag differences' do
+      diffs = result.diffs
+      expect(diffs.size).to eq(0)
+    end
   end
 
   context 'with --include-tags not specified' do
+    let(:argv) do
+      default_argv
+    end
+
+    let(:result) do
+      OctocatalogDiff::Integration.integration(
+        argv: argv
+      )
+    end
+
+    it 'should exit indicating success with no differences' do
+      expect(result.exitcode).to eq(0)
+    end
+
+    it 'should contain representative tag differences' do
+      diffs = result.diffs
+      expect(diffs.size).to eq(0)
+    end
   end
 
   context 'with --ignore-tags and --include-tags specified' do
+    let(:argv) do
+      default_argv.concat ['--include-tags', '--ignore-tags', 'foo']
+    end
+
+    let(:result) do
+      OctocatalogDiff::Integration.integration(
+        argv: argv
+      )
+    end
+
+    it 'should exit indicating success with differences' do
+      expect(result.exitcode).to eq(2)
+    end
+
+    it 'should contain representative tag differences' do
+      diffs = result.diffs
+      expect(diffs.size).to eq(1)
+      expect(diffs.first.change?).to eq(true)
+      expect(diffs.first.structure).to eq(['tags'])
+      expect(diffs.first.old_value).to eq(['tag-one', 'tag-two'])
+      expect(diffs.first.new_value).to eq(['ignore-tag__file', 'tag-one-new', 'tag-too'])
+    end
   end
 
   context 'with --ignore-tags and --no-include-tags specified' do
+    let(:argv) do
+      default_argv.concat ['--no-include-tags', '--ignore-tags', 'foo']
+    end
+
+    let(:result) do
+      OctocatalogDiff::Integration.integration(
+        argv: argv
+      )
+    end
+
+    it 'should exit indicating success with no differences' do
+      expect(result.exitcode).to eq(0)
+    end
+
+    it 'should contain representative tag differences' do
+      diffs = result.diffs
+      expect(diffs.size).to eq(0)
+    end
+  end
+
+  context 'with matching --ignore-tags and --include-tags specified' do
+    let(:argv) do
+      default_argv.concat ['--include-tags', '--ignore-tags', 'ignore-tag']
+    end
+
+    let(:result) do
+      OctocatalogDiff::Integration.integration(
+        argv: argv
+      )
+    end
+
+    it 'should exit indicating success with no differences' do
+      expect(result.exitcode).to eq(0)
+    end
+
+    it 'should contain representative tag differences' do
+      diffs = result.diffs
+      expect(diffs.size).to eq(0)
+    end
   end
 end
