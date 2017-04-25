c0906ef4d8d8c6365a7c464aad5a53fa437bebf6
Kevin Paulisse <kpaulisse@github.com>
2017-04-25 17:12:57 -0400

    Create fixtures and first integration test

--- spec/octocatalog-diff/integration/include_tags_spec.rb
+++ spec/octocatalog-diff/integration/include_tags_spec.rb
@@ -4,7 +4,36 @@ require_relative 'integration_helper'
 require 'json'
 
 describe 'include-tags integration' do
+  let(:default_argv) do
+    [
+      '--from-catalog', OctocatalogDiff::Spec.fixture_path('catalogs/include-tags-old.json'),
+      '--to-catalog', OctocatalogDiff::Spec.fixture_path('catalogs/include-tags-new.json')
+    ]
+  end
+
   context 'with --include-tags specified' do
+    let(:argv) do
+      default_argv.concat ['--include-tags']
+    end
+
+    let(:result) do
+      OctocatalogDiff::Integration.integration(
+        argv: argv
+      )
+    end
+
+    it 'should exit indicating success with differences' do
+      expect(result.exitcode).to eq(2)
+    end
+
+    it 'should contain representative tag differences' do
+      diffs = result.diffs
+      expect(diffs.size).to eq(1)
+      expect(diffs.first.change?).to eq(true)
+      expect(diffs.first.structure).to eq(['tags'])
+      expect(diffs.first.old_value).to eq(['tag-one', 'tag-two'])
+      expect(diffs.first.new_value).to eq(['tag-one-new', 'tag-too'])
+    end
   end
 
   context 'with --no-include-tags specified' do
