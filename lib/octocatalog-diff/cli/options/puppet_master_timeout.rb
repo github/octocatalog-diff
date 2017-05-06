10b9fd8a3d468e3341e912513972625a9f9794a3
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-05-06 10:46:49 -0500

    Grammar fixes from prior search/replace

--- lib/octocatalog-diff/cli/options/puppet_master_timeout.rb
+++ lib/octocatalog-diff/cli/options/puppet_master_timeout.rb
@@ -14,7 +14,7 @@ OctocatalogDiff::Cli::Options::Option.newoption(:puppet_master_timeout) do
       cli_name: 'puppet-master-timeout',
       option_name: 'puppet_master_timeout',
       desc: 'Puppet Master catalog retrieval timeout in seconds',
-      validator: ->(x) { x.to_i > 0 || raise(ArgumentError, 'Specify timeout as a integer greater than 0') },
+      validator: ->(x) { x.to_i > 0 || raise(ArgumentError, 'Specify timeout as an integer greater than 0') },
       translator: ->(x) { x.to_i }
     )
   end
