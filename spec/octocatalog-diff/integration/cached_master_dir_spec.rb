763ccca6d0cbed117a9bcdd1cd5d89f62ce7ba08
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-05-09 09:11:58 -0500

    This test can be performed in parallel mode

--- spec/octocatalog-diff/integration/cached_master_dir_spec.rb
+++ spec/octocatalog-diff/integration/cached_master_dir_spec.rb
@@ -197,8 +197,7 @@ describe 'cached master directory' do
           '--cached-master-dir', @cached_master_dir,
           '--safe-to-delete-cached-master-dir', @cached_master_dir,
           '--master-cache-branch', 'master',
-          '-n', 'rspec-node.github.net',
-          '--no-parallel'
+          '-n', 'rspec-node.github.net'
         ]
       )
 
