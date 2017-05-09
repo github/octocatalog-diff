d432af25a81cd5cca9a7f4436c7b725f42347f44
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-05-09 09:00:10 -0500

    Remove some debugging

--- spec/octocatalog-diff/tests/util/parallel_spec.rb
+++ spec/octocatalog-diff/tests/util/parallel_spec.rb
@@ -75,15 +75,15 @@ describe OctocatalogDiff::Util::Parallel do
 
       one_result = result[0]
       expect(one_result).to be_a_kind_of(OctocatalogDiff::Util::Parallel::Result)
-      expect(one_result.status).to eq(true), result.inspect
-      expect(one_result.exception).to eq(nil), result.inspect
-      expect(one_result.output).to eq('one abc'), result.inspect
+      expect(one_result.status).to eq(true)
+      expect(one_result.exception).to eq(nil)
+      expect(one_result.output).to eq('one abc')
 
       two_result = result[1]
       expect(two_result).to be_a_kind_of(OctocatalogDiff::Util::Parallel::Result)
-      expect(two_result.status).to eq(false), result.inspect
-      expect(two_result.exception).to be_a_kind_of(RuntimeError), result.inspect
-      expect(two_result.exception.message).to eq('Two failed'), result.inspect
+      expect(two_result.status).to eq(false)
+      expect(two_result.exception).to be_a_kind_of(RuntimeError)
+      expect(two_result.exception.message).to eq('Two failed')
     end
 
     it 'should kill running tasks when one task fails' do
