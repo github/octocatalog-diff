35e8d6ad573ad3b3c7c4650d80924c8a9d90b95d
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-06-05 20:18:38 -0500

    Don't need fixnum here either

--- spec/octocatalog-diff/tests/util/util_spec.rb
+++ spec/octocatalog-diff/tests/util/util_spec.rb
@@ -8,13 +8,13 @@ describe OctocatalogDiff::Util::Util do
   describe '#object_is_any_of?' do
     it 'should return true when object is one of the classes' do
       object = 42
-      classes = [Fixnum, Integer]
+      classes = [String, Hash, Integer]
       expect(described_class.object_is_any_of?(object, classes)).to eq(true)
     end
 
     it 'should return false when object is not one of the classes' do
       object = :chickens
-      classes = [Fixnum, Integer]
+      classes = [String, Hash, Integer]
       expect(described_class.object_is_any_of?(object, classes)).to eq(false)
     end
   end
