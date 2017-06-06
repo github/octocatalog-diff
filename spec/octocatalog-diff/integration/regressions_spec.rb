155d62ee49922890caa792ead176f911377aa29d
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-06-05 19:48:42 -0500

    Create test and fixtures demonstrating Fixnum regression

--- spec/octocatalog-diff/integration/regressions_spec.rb
+++ spec/octocatalog-diff/integration/regressions_spec.rb
@@ -25,3 +25,18 @@ describe 'miscellaneous regressions' do
     expect(@result[:diffs].size).to eq(0), @result[:diffs].map(&:inspect).join("\n")
   end
 end
+
+describe 'resource with Fixnum parameters added' do
+  before(:all) do
+    @result = OctocatalogDiff::Integration.integration(
+      from_catalog: OctocatalogDiff::Spec.fixture_path('catalogs/regression-fixnum-1.json'),
+      to_catalog: OctocatalogDiff::Spec.fixture_path('catalogs/regression-fixnum-2.json')
+    )
+  end
+
+  it 'should run without an error' do
+    expect(@result[:exitcode]).not_to eq(-1), "Internal error: #{OctocatalogDiff::Integration.format_exception(@result)}"
+    expect(@result[:exitcode]).to eq(2), "Runtime error: #{@result[:logs]}"
+    expect(@result[:diffs].size).to eq(1), @result[:diffs].map(&:inspect).join("\n")
+  end
+end
