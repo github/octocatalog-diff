1de751fa82a13624542ef4f9a099f51d8d555aaf
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-10-13 08:12:01 -0700

    Add integration for puppet 3 + hiera 5

--- spec/octocatalog-diff/fixtures/repos/hiera5/modules/test/manifests/init.pp
+++ spec/octocatalog-diff/fixtures/repos/hiera5/modules/test/manifests/init.pp
@@ -1,7 +1,7 @@
 class test (
-  String $param_from_nodes   = 'hard-coded',
-  String $param_from_special = 'hard-coded',
-  String $param_from_common  = 'hard-coded'
+  $param_from_nodes   = 'hard-coded',
+  $param_from_special = 'hard-coded',
+  $param_from_common  = 'hard-coded'
 ) {
   file { '/tmp/nodes': content => $param_from_nodes }
   file { '/tmp/special': content => $param_from_special }
--- spec/octocatalog-diff/integration/hiera5_spec.rb
+++ spec/octocatalog-diff/integration/hiera5_spec.rb
@@ -20,27 +20,52 @@ describe 'repository with hiera 5' do
   end
 
   context 'with --hiera-path-strip and per-item data directory' do
-    it 'should succeed in building the catalog' do
-      argv = ['-n', 'rspec-node.github.net', '--hiera-path-strip', '/var/lib/puppet']
-      hash = {
-        hiera_config: 'config/hiera5-global.yaml',
-        spec_fact_file: 'facts.yaml',
-        spec_repo: 'hiera5',
-        spec_catalog_old: 'catalog-empty.json'
-      }
-      result = OctocatalogDiff::Integration.integration(hash.merge(argv: argv))
-      expect(result.exitcode).to eq(2), OctocatalogDiff::Integration.format_exception(result)
+    if ENV['PUPPET_VERSION'].start_with?('3')
+      it 'should run but not use hiera values under puppet 3' do
+        argv = ['-n', 'rspec-node.github.net', '--hiera-path-strip', '/var/lib/puppet']
+        hash = {
+          hiera_config: 'config/hiera5-global.yaml',
+          spec_fact_file: 'facts.yaml',
+          spec_repo: 'hiera5',
+          spec_catalog_old: 'catalog-empty.json'
+        }
+        result = OctocatalogDiff::Integration.integration(hash.merge(argv: argv))
+        expect(result.exitcode).to eq(2), OctocatalogDiff::Integration.format_exception(result)
 
-      to_catalog = result.to
+        to_catalog = result.to
 
-      param1 = { 'content' => 'Greets from nodes' }
-      expect(to_catalog.resource(type: 'File', title: '/tmp/nodes')['parameters']).to eq(param1)
+        param1 = { 'content' => 'hard-coded' }
+        expect(to_catalog.resource(type: 'File', title: '/tmp/nodes')['parameters']).to eq(param1)
 
-      param2 = { 'content' => 'Greets from special' }
-      expect(to_catalog.resource(type: 'File', title: '/tmp/special')['parameters']).to eq(param2)
+        param2 = { 'content' => 'hard-coded' }
+        expect(to_catalog.resource(type: 'File', title: '/tmp/special')['parameters']).to eq(param2)
 
-      param3 = { 'content' => 'Greets from common' }
-      expect(to_catalog.resource(type: 'File', title: '/tmp/common')['parameters']).to eq(param3)
+        param3 = { 'content' => 'hard-coded' }
+        expect(to_catalog.resource(type: 'File', title: '/tmp/common')['parameters']).to eq(param3)
+      end
+    else
+      it 'should succeed in building the catalog' do
+        argv = ['-n', 'rspec-node.github.net', '--hiera-path-strip', '/var/lib/puppet']
+        hash = {
+          hiera_config: 'config/hiera5-global.yaml',
+          spec_fact_file: 'facts.yaml',
+          spec_repo: 'hiera5',
+          spec_catalog_old: 'catalog-empty.json'
+        }
+        result = OctocatalogDiff::Integration.integration(hash.merge(argv: argv))
+        expect(result.exitcode).to eq(2), OctocatalogDiff::Integration.format_exception(result)
+
+        to_catalog = result.to
+
+        param1 = { 'content' => 'Greets from nodes' }
+        expect(to_catalog.resource(type: 'File', title: '/tmp/nodes')['parameters']).to eq(param1)
+
+        param2 = { 'content' => 'Greets from special' }
+        expect(to_catalog.resource(type: 'File', title: '/tmp/special')['parameters']).to eq(param2)
+
+        param3 = { 'content' => 'Greets from common' }
+        expect(to_catalog.resource(type: 'File', title: '/tmp/common')['parameters']).to eq(param3)
+      end
     end
   end
 end
