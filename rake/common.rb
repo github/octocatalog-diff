d100250a3493f3c2c61e2ea8da53358392d12267
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-11-15 17:16:57 -0800

    Correct pattern arg for rspec

--- rake/common.rb
+++ rake/common.rb
@@ -3,7 +3,7 @@ BASEDIR = File.expand_path('..', File.dirname(__FILE__)).freeze
 PUPPET_BINARY = File.join(BASEDIR, 'bin', 'puppet').freeze
 TEST_COMMAND = begin
   if ENV['TRAVIS']
-    "rspec --suffix '_spec.rb$'"
+    "rspec --pattern '*_spec.rb'"
   else
     "parallel_rspec --suffix '_spec.rb$'"
   end
