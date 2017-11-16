e883cedd6c0e43a4a90d0cc1b862be2c412a1096
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-11-15 17:06:00 -0800

    Don't use parallel_tests under travis

--- rake/common.rb
+++ rake/common.rb
@@ -1,4 +1,10 @@
 # Constants
 BASEDIR = File.expand_path('..', File.dirname(__FILE__)).freeze
 PUPPET_BINARY = File.join(BASEDIR, 'bin', 'puppet').freeze
-TEST_COMMAND = "parallel_rspec --suffix '_spec.rb$'".freeze
+TEST_COMMAND = begin
+  if ENV['TRAVIS']
+    "rspec --suffix '_spec.rb$'"
+  else
+    "parallel_rspec --suffix '_spec.rb$'"
+  end
+end.freeze
