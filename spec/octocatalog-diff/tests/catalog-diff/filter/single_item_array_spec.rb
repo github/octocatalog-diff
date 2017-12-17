5832a3bd0c7b81b270b26a63a3c9299d1cc88aad
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2017-12-17 09:10:49 -0600

    Start rspec unit test for filter

---
+++ spec/octocatalog-diff/tests/catalog-diff/filter/single_item_array_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require_relative '../../spec_helper'
+require OctocatalogDiff::Spec.require_path('/api/v1/diff')
+require OctocatalogDiff::Spec.require_path('/catalog-diff/filter/single_item_array')
+
+describe OctocatalogDiff::CatalogDiff::Filter::SingleItemArray do
+  let(:subject) { described_class.new }
+
+  describe '#filtered?' do
+    it 'should not filter out an added resource' do
+      diff = ['+', "File\ffoobar.json", { 'parameters' => { 'content' => '{"foo":"bar"}' } }]
+      diff_obj = OctocatalogDiff::API::V1::Diff.new(diff)
+      result = subject.filtered?(diff_obj)
+      expect(result).to eq(false)
+    end
+
+    it 'should not filter out a removed resource' do
+      diff = ['-', "File\ffoobar.json", { 'parameters' => { 'content' => '{"foo":"bar"}' } }]
+      diff_obj = OctocatalogDiff::API::V1::Diff.new(diff)
+      result = subject.filtered?(diff_obj)
+      expect(result).to eq(false)
+    end
+
+    it 'should filter when from-catalog has string and to-catalog has array with that string' do
+      diff = [
+        '~',
+        "File\ffoobar.json",
+        { 'parameters' => { 'notify' => 'Service[foo]' } },
+        { 'parameters' => { 'notify' => ['Service[foo]'] } }
+      ]
+      diff_obj = OctocatalogDiff::API::V1::Diff.new(diff)
+      result = subject.filtered?(diff_obj)
+      expect(result).to eq(true)
+    end
+  end
+end
