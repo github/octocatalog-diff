a39f5614883b005e47cc7f670d1a0066e9ccff83
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2021-02-12 09:55:32 -0600

    Handle JSON facts structured as name/values

--- spec/octocatalog-diff/tests/facts/json_spec.rb
+++ spec/octocatalog-diff/tests/facts/json_spec.rb
@@ -39,5 +39,30 @@ describe OctocatalogDiff::Facts::JSON do
       expect(result['name']).to eq('override.node')
       expect(result['values']['fqdn']).to eq('rspec-node.xyz.github.net')
     end
+
+    it 'should fill in a missing name from hash with name/values' do
+      fact_file = OctocatalogDiff::Spec.fixture_path('facts/encoded-facts.json')
+      data = JSON.parse(File.read(fact_file))
+      data['name'] = ''
+      options = {
+        fact_file_string: JSON.generate(data)
+      }
+      result = OctocatalogDiff::Facts::JSON.fact_retriever(options)
+      expect(result).to be_a_kind_of(Hash)
+      expect(result['name']).to eq('rspec-node.xyz.github.net')
+      expect(result['values']['fqdn']).to eq('rspec-node.xyz.github.net')
+    end
+
+    it 'should not double-encode hash already containing name and values' do
+      fact_file = OctocatalogDiff::Spec.fixture_path('facts/encoded-facts.json')
+      options = {
+        fact_file_string: File.read(fact_file)
+      }
+      result = OctocatalogDiff::Facts::JSON.fact_retriever(options)
+      expect(result).to be_a_kind_of(Hash)
+      expect(result.keys).to match_array(%w(name values))
+      expect(result['name']).to eq('rspec-node.abc.github.net')
+      expect(result['values']['fqdn']).to eq('rspec-node.xyz.github.net')
+    end
   end
 end
