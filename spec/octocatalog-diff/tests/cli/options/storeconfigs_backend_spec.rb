baacd69b71074da7405b7ccaaf4c53e3b108c372
Nick Grundy <ngrundy@users.noreply.github.com>
2024-09-13 08:35:23 +1000

    tweak storeconfigs_backend_spec

--- spec/octocatalog-diff/tests/cli/options/storeconfigs_backend_spec.rb
+++ spec/octocatalog-diff/tests/cli/options/storeconfigs_backend_spec.rb
@@ -5,8 +5,8 @@ require_relative '../options_helper'
 describe OctocatalogDiff::Cli::Options do
   describe '#opt_storeconfigs_backend' do
     it 'should accept all valid arguments' do
-      result = run_optparse(['--storeconfigs-backend', 'puppetdb,yaml,json'])
-      expect(result[:validate_references]).to eq(%w(puppetdb yaml json))
+      result = run_optparse(['--storeconfigs-backend', 'puppetdb'])
+      expect(result[:storeconfigs_backend]).to eq('puppetdb')
     end
   end
 end
